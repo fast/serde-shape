0cceb92b79b582b268dfe974229931c8a2c2eaac
tison <wander4096@gmail.com>
2026-06-23 00:15:27 +0800

    for release
    Signed-off-by: tison <wander4096@gmail.com>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! An xtask binary for managing workspace tasks.
-
 use std::path::Path;
 use std::process::Command as StdCommand;
 
